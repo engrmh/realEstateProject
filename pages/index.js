484b5ae7b447d3aa62c5521e13c5ca8f511f50c4
GALAHAD <59095451+engrmh@users.noreply.github.com>
2023-09-14 17:38:44 +0330

    refactor: home card in main page

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Features from '@/components/templates/Home/index/Features'
 import React from 'react'
 import Story from '@/components/templates/Home/index/Story'
 import Homes from '@/components/templates/Home/index/Homes'
+import Gallery from '@/components/templates/Home/index/Gallery'
 
 export default function index() {
   return (
@@ -9,6 +10,7 @@ export default function index() {
       <Features/>
       <Story/>
       <Homes/>
+      <Gallery/>
     </>
   )
 }
