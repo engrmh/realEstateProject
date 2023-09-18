fbe932f067b07345b1e0d0c9da028933a562acf9
GALAHAD <59095451+engrmh@users.noreply.github.com>
2023-09-18 13:53:23 +0330

    feat: all home page

--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,7 @@ import Homes from '@/components/templates/Home/index/Homes'
 import Gallery from '@/components/templates/Home/index/Gallery'
 
 export default function index() {
+
   return (
     <>
       <Features/>
